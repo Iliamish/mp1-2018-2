e09965b3cea985d8e4f495594791e590c5a3ee03
Ilya <pokiloda@mail.ru>
2018-10-21 13:04:02 +0300

    task6 fundamental error 1

--- mishin_ia/additionalTasks/task6/Project1/Project1/Source.cpp
+++ mishin_ia/additionalTasks/task6/Project1/Project1/Source.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <conio.h>
 
-#define N 6
-#define M 7
+#define N 3
+#define M 3
 
 int main() {
-	int q[N] = { -3,3,4,6,7,8 };
-	int b[M] = { -2,-1,0,2,4,7,9 };
+	int q[N] = { 4,5,6 };
+	int b[M] = { 1,2,3 };
 	int c[N+M];
 
 	int i = 0, j = 0;
 
 	for (int k = 0; k < N+M; k++)
 	{
-		if (((i<N) && (q[i]<b[j])) || (j > M-1))
+
+		if ((j > M - 1) || ((i<N) && (q[i] < b[j])))
 		{
 			c[k] = q[i];
 			i++;
